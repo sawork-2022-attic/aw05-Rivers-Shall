d03a8305f8f3d437ddbc42df3bec2237624d158a
Rivers-Shall <riversshall@qq.com>
2022-04-18 15:48:35 +0800

    (feat) apply ehcache

---
+++ pos-products/src/main/java/com/micropos/products/repository/JDParser.java
@@ -0,0 +1,41 @@
+package com.micropos.products.repository;
+
+import com.micropos.products.model.Product;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class JDParser {
+
+    @Cacheable(value = "JD", key = "#keyword")
+    public List<Product> parseJD(String keyword) throws IOException {
+        String url = "https://search.jd.com/Search?keyword=" + keyword;
+        Document document = Jsoup.parse(new URL(url), 10000);
+        Element element = document.getElementById("J_goodsList");
+        Elements elements = element.getElementsByTag("li");
+        List<Product> list = new ArrayList<>();
+
+        for (Element el : elements) {
+            String id = el.attr("data-spu");
+            String img = "https:".concat(el.getElementsByTag("img").eq(0).attr("data-lazy-img"));
+            String price = el.getElementsByAttribute("data-price").text();
+            String title = el.getElementsByClass("p-name").eq(0).text();
+            if (title.indexOf("，") >= 0)
+                title = title.substring(0, title.indexOf("，"));
+
+            Product product = new Product(id, title, Double.parseDouble(price), img);
+            list.add(product);
+        }
+        return list;
+    }
+}
